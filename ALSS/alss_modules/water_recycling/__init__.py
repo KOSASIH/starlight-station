93f2d8b23fa9704416613ed319b32b0f0173a5b0
KOSASIH <kosasihg88@gmail.com>
2024-07-26 19:15:33 +0700

    Create __init__.py

---
+++ ALSS/alss_modules/water_recycling/__init__.py
@@ -0,0 +1,7 @@
+**`water_recycling/__init__.py`**
+```python
+# Initialize the water recycling system package
+from .water_recycling_simulation import WaterRecyclingSimulation
+from .water_recycling_control import WaterRecyclingControl
+from .water_recycling_sensors import WaterRecyclingSensors
+from .water_recycling_actuators import WaterRecyclingActuators
