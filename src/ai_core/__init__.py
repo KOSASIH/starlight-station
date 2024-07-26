3a9352aa07b38c58dddd5f399fb6b553684534eb
KOSASIH <kosasihg88@gmail.com>
2024-07-26 14:14:20 +0700

    Create __init__.py

---
+++ src/ai_core/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+from .neural_network_module import NeuralNetworkModule
+from .deep_learning_module import DeepLearningModule
+from .natural_language_processing_module import NaturalLanguageProcessingModule
+from .robotics_control_module import RoboticsControlModule
+
+__all__ = [
+    "NeuralNetworkModule",
+    "DeepLearningModule",
+    "NaturalLanguageProcessingModule",
+    "RoboticsControlModule"
+]
