3ee8fcdd6b431bd7b0eb6bb458c947e0f9400b49
KOSASIH <kosasihg88@gmail.com>
2024-07-29 09:02:25 +0700

    Create __init__.py

---
+++ app/controllers/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Controllers Module
+
+This module contains controllers for the application.
+"""
+
+from.user_controller import UserController
+from.settings_controller import SettingsController
+from.data_controller import DataController
