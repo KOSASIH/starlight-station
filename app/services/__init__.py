0a357a4559bc226c70270f667b991ff378e6e852
KOSASIH <kosasihg88@gmail.com>
2024-07-29 08:53:54 +0700

    Create __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Services Module
+
+This module contains various services for the application.
+"""
+
+from .data_service import DataService
+from .logging_service import LoggingService
+from .notification_service import NotificationService
