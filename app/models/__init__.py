5ee8571cca7e2dc32855a564690be97234e29317
KOSASIH <kosasihg88@gmail.com>
2024-07-29 08:58:31 +0700

    Create __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Models Module
+
+This module contains database models for the application.
+"""
+
+from .user import User
+from .settings import Settings
+from .data import Data
